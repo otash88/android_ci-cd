041f558e4649dc89258816af12f8174912da5a26
Otabek <otabekchorshanbiyev5161@gnail.com>
2025-02-19 15:18:53 +0500

    feat: add error test

--- app/src/androidTest/java/uz/gita/androidcicd/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/uz/gita/androidcicd/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("uz.gita.androidcicd", appContext.packageName)
+        assertEquals("uz.gita.androidcicdotash", appContext.packageName)
     }
 }
